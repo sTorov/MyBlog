ca183179277c949ada0f098f0d5d24338df42ddd
sTorov <sotorov2011@yandex.ru>
2023-05-13 05:33:20 +0300

    Добавлены контроллеры в MyBlog.Api для всех бизнес-моделей

---
+++ MyBlog.Api/Controllers/TagApiController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace MyBlog.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public void Get([FromRoute] int id)
+        {
+
+        }
+
+        [HttpGet]
+        public void GetAll()
+        {
+        }
+
+        [HttpPost]
+        public void Create(/*[FromBody] model*/)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public void Update([FromRoute] int id /*[FromBody] model*/)
+        {
+
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete([FromRoute] int id)
+        {
+
+        }
+    }
+}
